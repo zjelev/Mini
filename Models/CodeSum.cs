c97a7cbeac4c929ca9639f2f879857598f337bc6
zjelev <zlatomir@gmail.com>
2022-09-15 18:53:37 +0300

    Add files via upload

---
+++ Models/CodeSum.cs
@@ -0,0 +1,30 @@
+namespace ExcelUtils
+{
+    class CodeSum
+    {
+        private int code;
+        private string name;
+        private decimal sum;
+        private decimal hours;
+
+        public CodeSum(int code, string name, decimal sum, decimal hours)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Sum = sum;
+            this.Hours = hours;
+        }
+
+        public CodeSum(int code, decimal sum, decimal hours)
+        {
+            this.Code = code;
+            this.Sum = sum;
+            this.Hours = hours;
+        }
+
+        public int Code { get => code; private set => code = value; }
+        public string Name { get => name; private set => name = value; }
+        public decimal Sum { get => sum; set => sum = value; }
+        public decimal Hours { get => hours; set => hours = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ExcelUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            string currentDirectory = Environment.CurrentDirectory;
+
+            string[] directories = Directory.GetDirectories(currentDirectory);
+
+            foreach (string dir in directories)
+            {
+                if (!(dir.EndsWith("bin") || dir.EndsWith("obj") || dir.EndsWith(".vscode") || dir.EndsWith("Models") || dir.ToLower().EndsWith("weightnotes")))
+                {
+                    string[] subDirectories = Directory.GetDirectories(dir);
+                    foreach (string subDir in subDirectories)
+                    {
+                        List<string> xlsxFiles = Directory.GetFiles(subDir, "*.xlsx").ToList();
+                        if (xlsxFiles.Count > 0)
+                        {
+                            var codesSums = new Dictionary<(int, string), CodeSum>();
+                            foreach (string file in xlsxFiles)
+                            {
+                                CodeSumsTotal.AddHeaderToExistingFile(file);
+                                CodeSumsTotal.AddSumsByCode(file, codesSums);
+                            }
+                            Console.WriteLine("В директория {0} бяха обработени {1} файла.", subDir, xlsxFiles.Count);
+                            string upperFolderName = new DirectoryInfo(Path.GetDirectoryName(subDir)).Name;
+                            CodeSumsTotal.InsertCodesSumsInXlsx(codesSums, subDir + "_Recap_" + upperFolderName.ToUpper() + "_MMI.xlsx");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
