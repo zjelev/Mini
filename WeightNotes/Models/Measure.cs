a13747608776f4064231eb1df7cd07d818bb755c
zjelev <zlatomir@gmail.com>
2022-10-25 14:07:01 +0300

    ignore file names

--- WeightNotes/Models/Measure.cs
+++ WeightNotes/Models/Measure.cs
@@ -1,56 +1,46 @@
 using System.Globalization;
 
-namespace WeightNotes
+public class Measure
 {
-    public class Measure
+    public Measure(int id, DateTime brutoTime)
     {
-        public Measure()
-        {
-            
-        }
-        public Measure(int protokolNum, string tractorNum, string regNum, string driver, string egn, string phone, DateTime fromDate  )
-        {
-            this.ProtokolNum = protokolNum;
-            this.TractorNum = tractorNum;
-            this.RegNum = regNum;
-            this.Driver = driver;
-            this.Egn = egn;
-            this.Phone = phone;
-            this.FromDate = fromDate;
-        }
-        public Measure(int id, int protokolNum, DateTime fromdate, string regNum, int bruto, 
-            string brutoHour, int tara, string taraHour, int netto)
-        {
-            this.Id = id;
-            this.ProtokolNum = protokolNum;
-            this.FromDate = fromdate;
-            this.RegNum = regNum;
-            this.Bruto = bruto;
-            this.BrutoHour = brutoHour;
-            this.Tara = tara;
-            this.TaraHour = taraHour;
-            this.Netto = netto;
-        }
-
-        public int Id { get; set; }
-        public int ProtokolNum { get; set; }
-        public DateTime FromDate { get; set; }
-        public string? RegNum { get; set; }
-        public int Bruto { get; set; }
-        public string? BrutoHour { get; set; }
-        public int Tara { get; set; }
-        public string? TaraHour { get; set; }
-        public int Netto { get; set; }
-
-        public string? TractorNum { get; set; }
-        public string? Driver { get; set; }
-        public string? Egn { get; set; }
-        public string? Phone { get; set; }
-
-        public override string ToString()
-        {
-            return $"{this.Id} ; {this.ProtokolNum} ; {this.FromDate.ToString("dd/MM/yy", CultureInfo.InvariantCulture)} ; {this.RegNum} ;"
-                  +$"{this.Bruto} ; {this.BrutoHour} ; {this.Tara} ; {this.TaraHour} ; {this.Netto}";
-        }
+        this.Id = id;
+        this.BrutoTime = brutoTime;
     }
+    public Measure(int id, DateTime brutoTime, string tractorNum, string regNum, string driver, string egn, string phone)
+    {
+        this.Id = id;
+        this.BrutoTime = brutoTime;
+        this.TractorNum = tractorNum;
+        this.RegNum = regNum;
+        this.Driver = driver;
+        this.Egn = egn;
+        this.Phone = phone;
+    }
+    public Measure(int num, int id, DateTime brutoTime, string regNum, int bruto, int tara)
+    {
+        this.Num = num;
+        this.Id = id;
+        this.BrutoTime = brutoTime;
+        this.RegNum = regNum;
+        this.Bruto = bruto;
+        this.Tara = tara;
+    }
+
+    public int Num { get; }
+    public int Id { get; }
+    public DateTime BrutoTime { get;}
+    public string RegNum { get;}
+    public int Bruto { get; }
+    public int Tara { get; }
+
+    public int Neto => Bruto - Tara;
+
+    public string? TractorNum { get; }
+    public string? Driver { get; }
+    public string? Egn { get; }
+    public string? Phone { get; }
+
+    public string TimeRegNum => $"{this.Num} ; {this.BrutoTime.ToString("dd/MM/yy", CultureInfo.InvariantCulture)} ; {this.RegNum}";
+    public string BrutoNeto => $"{this.Bruto} ; {this.Id} ; {this.Neto}";
 }
