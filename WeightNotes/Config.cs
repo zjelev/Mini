878aaf513cf7b359f480ba8fe57f2cb2f51b67ea
zjelev <zlatomir@gmail.com>
2022-10-30 07:06:01 +0200

    Revert "NPOICore -> NPOI"
    This reverts commit 9e0f4852d03c69b4f26be668653bd3b511c97a41.

--- WeightNotes/Config.cs
+++ WeightNotes/Config.cs
@@ -7,10 +7,7 @@ public class Config
     internal static string veznaHost = JsonSerializer.Deserialize<Config>(config).Vezna.Host;
     internal static string veznaPath = JsonSerializer.Deserialize<Config>(config).Vezna.Path;
     internal static string veznaFilePattern = JsonSerializer.Deserialize<Config>(config).Vezna.File;
-    internal static string supplier = JsonSerializer.Deserialize<Config>(config).Speditor.Supplier;
-    internal static string destination = JsonSerializer.Deserialize<Config>(config).Speditor.Destination;
-    internal static string client = JsonSerializer.Deserialize<Config>(config).Speditor.Client;
-    internal static string load = JsonSerializer.Deserialize<Config>(config).Speditor.Load;
+    internal static string clientInfo = JsonSerializer.Deserialize<Config>(config).Speditor.StaticInfo;
     internal static string speditorFile = JsonSerializer.Deserialize<Config>(config).Speditor.File;
     internal static TimeSpan beginShift = new TimeSpan(8, 0, 0);
     internal static TimeSpan endShift = new TimeSpan(20, 0, 0);
@@ -30,8 +27,5 @@ public class Speditor
 {
     public string File { get; set; }
     public string Email { get; set; }
-    public string Supplier { get; set; }
-    public string Destination { get; set; }
-    public string Client { get; set; }
-    public string Load { get; set; }
+    public string StaticInfo { get; set; }
 }
