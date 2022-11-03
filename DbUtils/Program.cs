451938f25f62aec1b7ca17c0ff433c74e2c619dc
zjelev <zlatomir@gmail.com>
2022-11-03 13:40:27 +0200

    enable ImplicitUsings

--- DbUtils/Program.cs
+++ DbUtils/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Globalization;
-using System.IO;
 using System.IO.Compression;
 using Utils;
 // using Microsoft.SqlServer.Management.Common;
