d7cd414a70546d1a86a6a39303d353a2c0584d19
zjelev <zlatomir@gmail.com>
2022-11-03 13:36:45 +0200

    new net6.0 program main style

--- CodeSums/Controller.cs
+++ CodeSums/Controller.cs
@@ -1,97 +1,92 @@
 using System.Data;
-using Utils;
 using OfficeOpenXml;
 using OfficeOpenXml.Style;
 
-namespace CodeSums
+public class Controller
 {
-    public class Controller
+    public static void AddHeader(string file)
     {
-        public static void AddHeaderToExistingFile(string file)
+        string fileName, month, podName, podNumberStr;
+        int year;
+        Util.GetHeader(file, out fileName, out year, out month, out podName, out podNumberStr);
+
+        using (ExcelPackage package = new ExcelPackage(new FileInfo(file)))
         {
-            string fileName, month, podName, podNumberStr;
-            int year;
-            TextFile.GetHeaderFromLeon(file, out fileName, out year, out month, out podName, out podNumberStr);
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.First(); //select sheet here
 
-            using (ExcelPackage package = new ExcelPackage(new FileInfo(file)))
+            string? a1 = Convert.ToString(worksheet.Cells[1, 1].Value);
+            if (a1 == "Код")
             {
-                ExcelWorksheet worksheet = package.Workbook.Worksheets.First(); //select sheet here
-                // int totalRows = worksheet.Dimension.End.Row;
-                // int totalColumns = worksheet.Dimension.End.Column;
-                string? a1 = Convert.ToString(worksheet.Cells[1, 1].Value);
-                if (a1 == "Код")
+                worksheet.InsertRow(1, 5);
+                worksheet.Cells[1, 2].Value = "ОБЩА";
+                //worksheet.Cells[1, 2].Style.Font.Bold = true;
+                worksheet.Cells[2, 2].Value = "СПРАВКА";
+                worksheet.Cells[3, 2].Value = $"НА {podName}{podNumberStr}";
+                string period = "МЕСЕЦ";
+                if (fileName.Length == 17)
                 {
-                    worksheet.InsertRow(1, 5);
-                    worksheet.Cells[1, 2].Value = "ОБЩА";
-                    //worksheet.Cells[1, 2].Style.Font.Bold = true;
-                    worksheet.Cells[2, 2].Value = "СПРАВКА";
-                    worksheet.Cells[3, 2].Value = $"НА {podName}{podNumberStr}";
-                    string period = "МЕСЕЦ";
-                    if (fileName.Length == 17)
-                    {
-                        month = String.Empty;
-                        period = "ГОДИНА";
-                    }
-                    worksheet.Cells[4, 2].Value = $"ЗА {period} {month}{year}";
-                    worksheet.Cells[5, 2].Value = $"";
-                    worksheet.Cells["B1:B5"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
-
-                    package.Save();
+                    month = String.Empty;
+                    period = "ГОДИНА";
                 }
-            }
-        }
+                worksheet.Cells[4, 2].Value = $"ЗА {period} {month}{year}";
+                worksheet.Cells[5, 2].Value = $"";
+                worksheet.Cells["B1:B5"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
 
-        internal static void AddSumsByCode(string file, Dictionary<(int, string), CodeSum> codesSums)
-        {
-            using (ExcelPackage package = new ExcelPackage(new FileInfo(file)))
-            {
-                ExcelWorksheet worksheet = package.Workbook.Worksheets.First();
-
-                for (int i = 7; i <= worksheet.Dimension.End.Row; i++)
-                {
-                    int code = Convert.ToInt32(worksheet.Cells[i, 1].Value);
-                    string? name = Convert.ToString(worksheet.Cells[i, 2].Value);
-                    decimal sum = Convert.ToDecimal(worksheet.Cells[i, 3].Value);
-                    decimal hours = Convert.ToDecimal(worksheet.Cells[i, 4].Value);
-                    (int, string) key = new (code, name!);
-                    CodeSum codeSum = new CodeSum(code, sum, hours);
-                    if (codesSums.ContainsKey(key))
-                    {
-                        codesSums[key].Sum += sum;
-                        codesSums[key].Hours += hours;
-                    }
-                    else
-                    {
-                        codesSums.Add(key, codeSum);
-                    }
-                }
-            }
-        }
-
-        internal static void InsertCodesSumsInXlsx(Dictionary<(int, string), CodeSum> codesSums, string fileName)
-        {
-            using (ExcelPackage package = new ExcelPackage(new FileInfo(fileName)))
-            {
-                DataTable dataTable = new DataTable();
-                dataTable.Columns.Add("Код", typeof(int));
-                dataTable.Columns.Add("Име", typeof(string));
-                dataTable.Columns.Add("Сума", typeof(decimal));
-                dataTable.Columns.Add("Часове", typeof(decimal));
-
-                foreach (var row in codesSums)
-                {
-                    dataTable.Rows.Add(row.Key.Item1, row.Key.Item2, row.Value.Sum, row.Value.Hours);
-                }
-
-                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Sheet 1");
-                //add all the content from the DataTable, starting at cell A1
-                worksheet.Cells["A1"].LoadFromDataTable(dataTable, true);
-
-                int rows = dataTable.Rows.Count;
-                int columns = dataTable.Columns.Count;
-                worksheet.Cells[2, 1, rows + 1, columns].Sort(new[] { 0, 1 }); // skip the header
                 package.Save();
             }
+        }
+    }
+
+    internal static void AddSumsByCode(string file, Dictionary<(int, string), CodeSum> codesSums)
+    {
+        using (ExcelPackage package = new ExcelPackage(new FileInfo(file)))
+        {
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.First();
+
+            for (int i = 7; i <= worksheet.Dimension.End.Row; i++)
+            {
+                int code = Convert.ToInt32(worksheet.Cells[i, 1].Value);
+                string? name = Convert.ToString(worksheet.Cells[i, 2].Value);
+                decimal sum = Convert.ToDecimal(worksheet.Cells[i, 3].Value);
+                decimal hours = Convert.ToDecimal(worksheet.Cells[i, 4].Value);
+                (int, string) key = new(code, name!);
+                CodeSum codeSum = new CodeSum(code, sum, hours);
+                if (codesSums.ContainsKey(key))
+                {
+                    codesSums[key].Sum += sum;
+                    codesSums[key].Hours += hours;
+                }
+                else
+                {
+                    codesSums.Add(key, codeSum);
+                }
+            }
+        }
+    }
+
+    internal static void InsertCodesSums(Dictionary<(int, string), CodeSum> codesSums, string fileName)
+    {
+        using (ExcelPackage package = new ExcelPackage(new FileInfo(fileName)))
+        {
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("Код", typeof(int));
+            dataTable.Columns.Add("Име", typeof(string));
+            dataTable.Columns.Add("Сума", typeof(decimal));
+            dataTable.Columns.Add("Часове", typeof(decimal));
+
+            foreach (var row in codesSums)
+            {
+                dataTable.Rows.Add(row.Key.Item1, row.Key.Item2, row.Value.Sum, row.Value.Hours);
+            }
+
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Sheet 1");
+            //add all the content from the DataTable, starting at cell A1
+            worksheet.Cells["A1"].LoadFromDataTable(dataTable, true);
+
+            int rows = dataTable.Rows.Count;
+            int columns = dataTable.Columns.Count;
+            worksheet.Cells[2, 1, rows + 1, columns].Sort(new[] { 0, 1 }); // skip the header
+            package.Save();
         }
     }
 }
--- CodeSums/Program.cs
+++ CodeSums/Program.cs
@@ -1,39 +1,28 @@
 ﻿using System.Text;
 
-namespace CodeSums
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+string[] directories = Directory.GetDirectories(Environment.CurrentDirectory);
+
+foreach (string dir in directories)
 {
-    class Program
+    if (!(dir.EndsWith("bin") || dir.EndsWith("obj") || dir.EndsWith(".vscode") || dir.EndsWith("Models")))
     {
-        static void Main(string[] args)
+        string[] subDirectories = Directory.GetDirectories(dir);
+        foreach (string subDir in subDirectories)
         {
-            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            string currentDirectory = Environment.CurrentDirectory;
-
-            string[] directories = Directory.GetDirectories(currentDirectory);
-
-            foreach (string dir in directories)
+            List<string> xlsxFiles = Directory.GetFiles(subDir, "*.xlsx").ToList();
+            if (xlsxFiles.Count > 0)
             {
-                if (!(dir.EndsWith("bin") || dir.EndsWith("obj") || dir.EndsWith(".vscode") || dir.EndsWith("Models") || dir.ToLower().EndsWith("weightnotes")))
+                var codesSums = new Dictionary<(int, string), CodeSum>();
+                foreach (string file in xlsxFiles)
                 {
-                    string[] subDirectories = Directory.GetDirectories(dir);
-                    foreach (string subDir in subDirectories)
-                    {
-                        List<string> xlsxFiles = Directory.GetFiles(subDir, "*.xlsx").ToList();
-                        if (xlsxFiles.Count > 0)
-                        {
-                            var codesSums = new Dictionary<(int, string), CodeSum>();
-                            foreach (string file in xlsxFiles)
-                            {
-                                Controller.AddHeaderToExistingFile(file);
-                                Controller.AddSumsByCode(file, codesSums);
-                            }
-                            Console.WriteLine("В директория {0} бяха обработени {1} файла.", subDir, xlsxFiles.Count);
-                            string? upperFolderName = new DirectoryInfo(Path.GetDirectoryName(subDir)!).Name;
-                            Controller.InsertCodesSumsInXlsx(codesSums, subDir + "_Recap_" + upperFolderName.ToUpper() + "_MMI.xlsx");
-                        }
-                    }
+                    Controller.AddHeader(file);
+                    Controller.AddSumsByCode(file, codesSums);
                 }
+                Console.WriteLine("В директория {0} бяха обработени {1} файла.", subDir, xlsxFiles.Count);
+                string? upperFolderName = new DirectoryInfo(Path.GetDirectoryName(subDir)!).Name;
+                Controller.InsertCodesSums(codesSums, subDir + "_Recap_" + upperFolderName.ToUpper() + "_MMI.xlsx");
             }
         }
     }
