7a379c0eb380af7bd161a262e495ac21fd52a2bf
zjelev <zlatomir@gmail.com>
2022-11-27 09:18:35 +0200

    Add dateFormat & paradoxDbPath

--- WeightNotes/Config.cs
+++ WeightNotes/Config.cs
@@ -4,14 +4,17 @@ public class Config
 {
     internal static string logPath = Environment.CurrentDirectory + Path.DirectorySeparatorChar + "bin" + Path.DirectorySeparatorChar;
     internal static string config = File.ReadAllText(logPath + "config.json");
-    internal static string veznaHost = JsonSerializer.Deserialize<Config>(config).Vezna.Host;
+    public static string veznaHost = JsonSerializer.Deserialize<Config>(config).Vezna.Host;
     internal static string veznaPath = JsonSerializer.Deserialize<Config>(config).Vezna.Path;
+    public static string veznaParadoxDb = JsonSerializer.Deserialize<Config>(config).Vezna.ParadoxDb;
     internal static string veznaFilePattern = JsonSerializer.Deserialize<Config>(config).Vezna.File;
     internal static string supplier = JsonSerializer.Deserialize<Config>(config).Speditor.Supplier;
     internal static string destination = JsonSerializer.Deserialize<Config>(config).Speditor.Destination;
     internal static string client = JsonSerializer.Deserialize<Config>(config).Speditor.Client;
     internal static string load = JsonSerializer.Deserialize<Config>(config).Speditor.Load;
     internal static string speditorFile = JsonSerializer.Deserialize<Config>(config).Speditor.File;
+    internal static string dateFormat = JsonSerializer.Deserialize<Config>(config).Speditor.DateFormat;
+
     internal static TimeSpan beginShift = new TimeSpan(8, 0, 0);
     internal static TimeSpan endShift = new TimeSpan(20, 0, 0);
 
@@ -24,6 +27,7 @@ public class Vezna
     public string Host { get; set; }
     public string Path { get; set; }
     public string File { get; set; }
+    public string ParadoxDb { get; set; }
 }
 
 public class Speditor
@@ -34,4 +38,5 @@ public class Speditor
     public string Destination { get; set; }
     public string Client { get; set; }
     public string Load { get; set; }
+    public string DateFormat { get; set; }
 }
