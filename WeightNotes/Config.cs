a65507b36ab1ad10741e2d1fc8e42b2d43941f63
zjelev <zlatomir@gmail.com>
2022-12-04 15:25:44 +0200

    add namespace

--- WeightNotes/Config.cs
+++ WeightNotes/Config.cs
@@ -5,17 +5,17 @@ public class Config
     public static string config = File.ReadAllText(Utils.Config.logPath + "config.json");
     public static string veznaHost = JsonSerializer.Deserialize<Config>(config).Vezna.Host;
     internal static string veznaPath = JsonSerializer.Deserialize<Config>(config).Vezna.Path;
-    public static string veznaParadoxDb = JsonSerializer.Deserialize<Config>(config).Vezna.ParadoxDb;
+    public static string veznaDb = JsonSerializer.Deserialize<Config>(config).Vezna.Db;
     internal static string veznaFilePattern = JsonSerializer.Deserialize<Config>(config).Vezna.File;
-    internal static string supplier = JsonSerializer.Deserialize<Config>(config).Speditor.Supplier;
-    internal static string destination = JsonSerializer.Deserialize<Config>(config).Speditor.Destination;
-    internal static string client = JsonSerializer.Deserialize<Config>(config).Speditor.Client;
-    internal static string load = JsonSerializer.Deserialize<Config>(config).Speditor.Load;
-    internal static string speditorFile = JsonSerializer.Deserialize<Config>(config).Speditor.File;
-    internal static string dateFormat = JsonSerializer.Deserialize<Config>(config).Speditor.DateFormat;
+    public static string supplier = JsonSerializer.Deserialize<Config>(config).Speditor.Supplier;
+    public static string destination = JsonSerializer.Deserialize<Config>(config).Speditor.Destination;
+    public static string client = JsonSerializer.Deserialize<Config>(config).Speditor.Client;
+    public static string load = JsonSerializer.Deserialize<Config>(config).Speditor.Load;
+    public static string speditorFile = JsonSerializer.Deserialize<Config>(config).Speditor.File;
+    public static string dateFormat = JsonSerializer.Deserialize<Config>(config).Speditor.DateFormat;
 
-    internal static TimeSpan beginShift = new TimeSpan(8, 0, 0);
-    internal static TimeSpan endShift = new TimeSpan(20, 0, 0);
+    public static TimeSpan beginShift = new TimeSpan(8, 0, 0);
+    public static TimeSpan endShift = new TimeSpan(20, 0, 0);
 
     public Vezna Vezna { get; set; }
     public Speditor Speditor { get; set; }
@@ -26,7 +26,7 @@ public class Vezna
     public string Host { get; set; }
     public string Path { get; set; }
     public string File { get; set; }
-    public string ParadoxDb { get; set; }
+    public string Db { get; set; }
 }
 
 public class Speditor
